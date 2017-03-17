7b861945a481af66e39a0db261e27ec868f6556c
Piotr Koscierzynski <Piotr.Koscierzynski@b-i.com>
2017-03-17 12:22:24 +0100

    Init

---
+++ AmazingApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AmazingApp
+//
+//  Created by Piotr Koscierzynski on 17/03/17.
+//
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
